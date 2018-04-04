6115697a13615d862755d1db3975c7fe28b93bb9
Sergey Golidonov <3apa3a86@inbox.ru>
2018-04-04 14:18:33 +0300

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -2,12 +2,20 @@ package ru.job4j.calculator;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
 
     @Test
     public void testAdd() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
     }
 
     @Test
