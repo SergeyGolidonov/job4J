29e1050205f151db5f3f3cf9d6ad42cbdb62062c
Sergey Golidonov <3apa3a86@inbox.ru>
2018-04-04 14:05:49 +0300

    3.1. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculator;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testGetResult() {
+    }
+}
