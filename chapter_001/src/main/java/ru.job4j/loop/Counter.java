46a3efe9f6510ea8de9d68f6ed71f1e3706cc57b
Sergey Golidonov <3apa3a86@inbox.ru>
2018-04-09 11:56:55 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- chapter_001/src/main/java/ru.job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru.job4j/loop/Counter.java
@@ -3,5 +3,13 @@ package ru.job4j.loop;
 public class Counter {
     public int add(int start, int finish) {
 
+        int a = 0;
+        for (int i=0; i<=10; i++) {
+            if (i%2==0) {
+                System.out.println(a*(a+1));
+                a++;
+            }
+        }
+        return a;
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    @Test
+    public void testAdd() {
+        Counter counter = new Counter();
+        int finish = counter.add(0, 10);
+        assertThat(finish, is(6));
+    }
+}
