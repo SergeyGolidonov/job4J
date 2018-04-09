8bf275115417bfc1a200176ee7d2ed798d9ab3f6
Sergey Golidonov <3apa3a86@inbox.ru>
2018-04-09 13:40:00 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- chapter_001/src/main/java/ru.job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru.job4j/loop/Factorial.java
@@ -6,6 +6,9 @@ public class Factorial {
         for (int i = 1; i < 5; i++) {
             result *=i;
             System.out.println(result);
+            if (n == 0) {
+                return 1;
+            }
         }
         return result;
     }
