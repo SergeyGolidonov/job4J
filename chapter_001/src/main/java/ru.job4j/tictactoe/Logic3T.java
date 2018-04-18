5f9f4570aeec02e552adace7371c9303a80e7d46
Sergey Golidonov <3apa3a86@inbox.ru>
2018-04-18 14:36:34 +0300

    Крестики-нолики на JavaFX [#49777]

---
+++ chapter_001/src/main/java/ru.job4j/tictactoe/Logic3T.java
@@ -0,0 +1,36 @@
+package ru.job4j.tictactoe;
+
+public class Logic3T {
+    private final Figure3T[][] table;
+
+    public Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+    public boolean isWinnerX() {
+        for(int i = 0; i < table.length; i++) {
+            if ((table[i][0] != table[i][1] && table[i][0] != table[i][2]) ||
+                    (table[0][i] != table[1][i] && table[0][i] != table[2][i]))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean isWinnerO() {
+        for(int i = 0; i < table.length; i++) {
+            if ((table[i][0] != table[i][1] && table[i][0] != table[i][2]) ||
+                    (table[0][i] != table[1][i] && table[0][i] != table[2][i]))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean hasGap() {
+        for(int i = 0; i < table.length; i++) {
+            if ((table[i][0] == table[i][1] && table[i][0] == table[i][2]) ||
+                    (table[0][i] == table[1][i] && table[0][i] == table[2][i]))
+                return false;
+        }
+        return true;
+    }
+}
