138e4d946766ab3b90f9b78aa1dd7f08eaba739e
Sergey Golidonov <3apa3a86@inbox.ru>
2018-08-05 18:27:55 +0300

    2. Написать программу преобразования List в Map. [#10093]

---
+++ chapter_003/src/main/java/ru.job4j/search/listToMap/UserConvert.java
@@ -0,0 +1,20 @@
+package ru.job4j.search.listToMap;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Sergey Golidonov (3apa3a86@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class UserConvert {
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> result = new HashMap<>();
+        for (User user : list) {
+            result.put(user.getId(), user);
+        }
+        return result;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/listToMap/UserConvertTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.search.listToMap;
+
+import org.junit.Test;
+import javax.jws.soap.SOAPBinding;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Sergey Golidonov (3apa3a86@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class UserConvertTest {
+    @Test
+    public void whenListConvertToMap() {
+        UserConvert userConvert = new UserConvert();
+        List<User> list = new ArrayList<>();
+        list.add(new User(12, "John", "NY"));
+        list.add(new User(11, "Bill", "Miami"));
+        list.add(new User(75, "Ilon Mask", "California"));
+        Map<Integer, User> result = userConvert.process(list);
+        HashMap<Integer, User> expected = new HashMap<>();
+        expected.put(12, new User(12, "John", "NY"));
+        expected.put(11, new User(11, "Bill", "Miami"));
+        expected.put(75, new User(75, "Ilon Mask", "California"));
+        assertThat(result, is(expected));
+    }
+}
