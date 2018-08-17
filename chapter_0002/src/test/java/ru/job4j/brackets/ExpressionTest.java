0c8ab3278ecf59e8100841fb972baf5978242fd3
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-08-17 12:06:39 +0300

    change package

--- chapter_0002/src/test/java/ru/job4j/brackets/ExpressionTest.java
+++ chapter_0002/src/test/java/ru/job4j/brackets/ExpressionTest.java
@@ -7,25 +7,29 @@ package ru.job4j.brackets;
  */
 
 import org.junit.Test;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
  * Тест для класса Expression. Распарсивание скобок.
  */
+
 public class ExpressionTest {
+
     /**
      * Скобки расставлены правильно.
      */
+
     @Test
     public void whenValidTrue() {
         Info result = Expression.parseBracket("a[b({c}d)e](fff)");
         assertThat(result.isValid(), is(true));
     }
+
     /**
      * Скобки расставлены неправильно.
      */
+
     @Test
     public void whenValidFalse() {
         Info result = Expression.parseBracket("a[b({c}d)e]");
