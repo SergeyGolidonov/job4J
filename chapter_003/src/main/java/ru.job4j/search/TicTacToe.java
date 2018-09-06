bc597fd20a56977a3fcff631e3110c4e20e5978f
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-09-06 12:05:05 +0300

    TicTacToe2.0

--- chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
+++ chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
@@ -1,7 +1,6 @@
 package ru.job4j.search;
 
 public class TicTacToe {
-    private int size;
     private final int[][] values;
 
     public TicTacToe(final int[][] values) {
@@ -15,25 +14,10 @@ public class TicTacToe {
                 checkWhenNobodyWins();
     }
 
-    private boolean checkWhenNobodyWins() {
-
-        for (int i = 0; i < size; i++) {
-            boolean res = true;
-            for (int j = 0; j < size && res; j++)
-                res = values[i][j] != values[i][0] &&
-                        values[j][i] != values[0][i] &&
-                        values[i][i] != values[0][0] &&
-                        values[size - j - 1][j] != values[size - 1][0];
-            if (res)
-                return false;
-        }
-        return true;
-    }
-
     private boolean checkWinnerHorizontal() {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < values.length; i++) {
             boolean res = true;
-            for (int j = 1; j < size && res; j++)
+            for (int j = 1; j < values.length; j++)
                 res = values[i][j] == values[i][0];
             if (res)
                 return true;
@@ -42,9 +26,9 @@ public class TicTacToe {
     }
 
     private boolean checkWinnerVertical() {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < values.length; i++) {
             boolean res = true;
-            for (int j = 1; j < size && res; j++)
+            for (int j = 1; j < values.length; j++)
                 res = values[j][i] == values[0][i];
             if (res)
                 return true;
@@ -54,13 +38,19 @@ public class TicTacToe {
 
     private boolean checkWinnerDiagonals() {
         boolean res = true;
-        for (int i = 1; i < size && res; i++)
-            res = values[i][i] == values[0][0];
-        if (res)
-            return true;
-        res = true;
-        for (int j = 1; j < size && res; j++)
-            res = values[size - j - 1][j] == values[size - 1][0];
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != values[i + 1]) {
+                res = false;
+                break;
+            }
+        }
         return res;
+    }
+
+    private boolean checkWhenNobodyWins() {
+        if (!checkWinnerVertical() & !checkWinnerHorizontal() & !checkWinnerDiagonals()) {
+            return true;
+        }
+        return false;
     }
 }
