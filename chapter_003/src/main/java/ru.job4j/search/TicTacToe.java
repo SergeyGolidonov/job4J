dcb27ab97b45af55b1c98aa4e6d58885cc2d789c
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-09-10 11:22:23 +0300

    TicTacToe2.0

--- chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
+++ chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
@@ -8,8 +8,7 @@ public class TicTacToe {
     }
 
     public boolean hasWinner() {
-        return isWinner() ||
-                hasGap();
+        return isWinner() || whenNobodyWins();
     }
 
     public boolean isWinner() {
@@ -22,13 +21,12 @@ public class TicTacToe {
         return false;
     }
 
-    public boolean hasGap() {
+    public boolean whenNobodyWins() {
         for (int i = 0; i < values.length; i++) {
-            if ((values[i][0] == values[i][1] && values[i][0] == values[i][2])
-                    || (values[0][i] == values[1][i] && values[0][i] == values[2][i])) {
-                return false;
+            if (!hasWinner()) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
