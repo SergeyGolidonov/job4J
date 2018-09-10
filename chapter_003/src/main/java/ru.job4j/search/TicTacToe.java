d29425b697f1951751690f9bf34f72e32f072b1c
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-09-10 10:43:50 +0300

    TicTacToe2.0

--- chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
+++ chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
@@ -8,53 +8,27 @@ public class TicTacToe {
     }
 
     public boolean hasWinner() {
-        return checkWinnerHorizontal() ||
-                checkWinnerVertical() ||
-                checkWinnerDiagonals() ||
-                checkWhenNobodyWins();
+        return isWinner() ||
+                hasGap();
     }
 
-    private boolean checkWinnerHorizontal() {
-        boolean res = true;
+    public boolean isWinner() {
         for (int i = 0; i < values.length; i++) {
-            for (int j = 0; j < values[i].length; j++) {
-                if (values[i][j] == values[i][0]) {
-                    res = true;
-                    break;
-                }
+            if ((values[i][0] != values[i][1] && values[i][0] != values[i][2])
+                    || (values[0][i] != values[1][i] && values[0][i] != values[2][i])) {
+                return true;
             }
-        }
-        return res;
-    }
-
-    private boolean checkWinnerVertical() {
-        boolean res = true;
-        for (int i = 0; i < values.length; i++) {
-            for (int j = 0; j < values[i].length; j++) {
-                if (values[i][j] == values[0][j]) {
-                    res = true;
-                    break;
-                }
-            }
-        }
-        return res;
-    }
-
-    private boolean checkWinnerDiagonals() {
-        boolean res = true;
-        for (int i = 0; i < values.length; i++) {
-            if (values[i] != values[i + 1]) {
-                res = false;
-                break;
-            }
-        }
-        return res;
-    }
-
-    private boolean checkWhenNobodyWins() {
-        if (!checkWinnerVertical() && !checkWinnerHorizontal() && !checkWinnerDiagonals()) {
-            return false;
         }
         return false;
+    }
+
+    public boolean hasGap() {
+        for (int i = 0; i < values.length; i++) {
+            if ((values[i][0] == values[i][1] && values[i][0] == values[i][2])
+                    || (values[0][i] == values[1][i] && values[0][i] == values[2][i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
