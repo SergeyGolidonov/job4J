d6802d23de0e0f2dc836d9fe226f0c9eb43060d7
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-09-13 09:47:39 +0300

    TicTacToe2.0

--- chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
+++ chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
@@ -23,11 +23,10 @@ public class TicTacToe {
 
     public boolean whenNobodyWins() {
         for (int i = 0; i < values.length; i++) {
-            if ((values[i][0] == values[i][1] && values[i][0] == values[i][2])
-                    || (values[0][i] == values[1][i] && values[0][i] == values[2][i])) {
-                return false;
+            if (!isWinner()) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
