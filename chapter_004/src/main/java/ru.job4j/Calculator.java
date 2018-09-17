73eea592b3de5ea062a685385e5209bd9e676e29
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-09-17 18:51:37 +0300

    0. Функциональные итерфейсы.  [#58165]

---
+++ chapter_004/src/main/java/ru.job4j/Calculator.java
@@ -0,0 +1,39 @@
+package ru.job4j;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+/**
+ * @author Sergey Golidonov (3apa3a86@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Calculator {
+
+    public interface Operation {
+        double calc(int left, int right);
+    }
+
+    public void multiple(int start, int finish, int value,
+                         BiFunction<Integer, Integer, Double> op,
+                         Consumer<Double> media) {
+        for (int index = start; index != finish; index++) {
+            media.accept(op.apply(value, index));
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        calc.multiple(
+                0, 10, 2,
+                (value, index) -> {
+                    double result = value * index;
+                    System.out.printf("Multiple %s * %s = %s %n", value, index, result);
+                    return result;
+                },
+                result -> System.out.println(result)
+        );
+    }
+}
---
+++ chapter_004/src/main/java/ru.job4j/Operation.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+/**
+ * @author Sergey Golidonov (3apa3a86@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public interface Operation {
+    double calc (int left, int right);
+}
