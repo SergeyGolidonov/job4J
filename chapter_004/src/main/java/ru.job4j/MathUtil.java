d1b82282e99b9daa920a37458bfc960f5b826845
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-09-18 08:43:36 +0300

    0.1 Ссылки на методы. [#70608]

---
+++ chapter_004/src/main/java/ru.job4j/MathUtil.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+/**
+ * @author Sergey Golidonov (3apa3a86@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class MathUtil {
+    public static double add(int left, int second) {
+        return left + second;
+    }
+
+    public static double div(int left, int second) {
+        return left / second;
+    }
+}
