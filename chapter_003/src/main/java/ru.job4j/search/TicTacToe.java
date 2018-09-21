c8444245d88bca347dd340dbd203426053b568b3
SergeyGolidonov <mailto:3apa3a86@inbox.ru>
2018-09-21 09:30:10 +0300

    TicTacToe2.0

--- chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
+++ chapter_003/src/main/java/ru.job4j/search/TicTacToe.java
@@ -8,12 +8,15 @@ public class TicTacToe {
     }
 
     public boolean hasWinner() {
-        return checkWinnerHorizont() || checkWinnerVertical();
+        return checkWinnerHorizont()
+                || checkWinnerVertical()
+                || checkWinnerDiagonal()
+                || checkWhenNobodyWins();
     }
 
     public boolean checkWinnerHorizont() {
         for (int i = 0; i < values.length; i++) {
-            for (int j= 0; j < values[i].length; j++) {
+            for (int j= 1; j < values.length; j++) {
                 if (values[i][0] == values[i][j]) {
                     return true;
                 }
@@ -24,10 +27,38 @@ public class TicTacToe {
 
     public boolean checkWinnerVertical() {
         for (int i = 0; i < values.length; i++) {
-            for (int j= 0; j < values[i].length; j++) {
+            for (int j= 1; j < values.length; j++) {
                 if (values[0][i] == values[i][j]) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    public boolean checkWinnerDiagonal() {
+        boolean result = true;
+        for (int i = 1; i < values.length; i++) {
+            if (values[0][0] != values[i][i]) {//проверяем первую диагональ
+                result = false;
+                break;
+            }
+        }
+        if (!result) {//если победителя нет то проверяем вторую
+            for (int i = 1; i < values.length; i++) {
+                if (values[0][values.length - 1] != values[i][values.length - 1 - i]) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean checkWhenNobodyWins() {
+        for (int i = 0; i < values.length; i++) {
+            if (!checkWinnerHorizont() && !checkWinnerVertical() && !checkWinnerDiagonal()) {
+                return true;
             }
         }
         return false;
